e9f156cac8d4019b172da10c22deaae9623c410c
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2025-10-11 13:52:55 +0200

    Estilos, colores, temas

--- app/src/main/java/com/example/playjuegos2/MainActivity.kt
+++ app/src/main/java/com/example/playjuegos2/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.playjuegos2
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,5 +24,20 @@ class MainActivity : AppCompatActivity() {
     fun lanzarNewPlayer(){
         val i= Intent(this, NewPlayer::class.java)
         startActivity(i)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.getItemId()
+
+        if (id ==R.id.action_buscar){
+            Toast.makeText(this,"Búsqueda", Toast.LENGTH_LONG).show();
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
